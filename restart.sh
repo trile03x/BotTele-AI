43c12e6609fd2c45d3b43592d98fa3b9e176dd9d
trile03x <levantri6839@gmail.com>
2024-06-18 16:49:48 +0700

    okek

--- restart.sh
+++ restart.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
+# Vòng lặp chạy trong 10 phút, mỗi lần nghỉ 5 giây
 for ((i=0; i<60; i++))
 do
     echo "Restarting dynos..."
-    web npm start
-    heroku ps:restart --app <tên-ứng-dụng-của-bạn>
-    sleep 5  # Thời gian nghỉ 5 giây giữa mỗi lần khởi động lại
+    # Chạy lệnh để khởi động ứng dụng
+    npm start  # Thay npm start bằng lệnh cần thiết để khởi động ứng dụng của bạn
+
+    # Đợi 5 giây
+    sleep 5
+
+    # Thực hiện lệnh heroku ps:restart
+    heroku ps:restart --app tritechzai
 done
